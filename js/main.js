738221de48e125e930ca0739818d3184c3bb02b6
teethmarks <teethmarks@gmail.com>
2016-12-02 01:47:43 +0800

    Made Reservations Section work!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,79 @@
+// Reservations
+
+    // PART ONE: INITIALIZE FIREBASE //
+    var config = {
+        apiKey: "AIzaSyBodgvzGNKpEsosjcqhsXG8OTPHrZD9c6Q",
+        authDomain: "reservation-site-7f3cf.firebaseapp.com",
+        databaseURL: "https://reservation-site-7f3cf.firebaseio.com",
+        storageBucket: "reservation-site-7f3cf.appspot.com",
+        messagingSenderId: "511253754930"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database(); // Connect to Database
+    var reservationData = {}; // Create Object which will be populated with user input
+
+    
+    // PART TWO: CREATE SECTION AND PUSH DATA //
+    $('.reservation-form').on('submit', function(event) { // On submit, name and date will be pushed to database
+        event.preventDefault();
+
+        var reservationName = $('.reservation-name').val();
+        var reservationDate = $('.reservation-date').val();
+        if (reservationName === '' || reservationDate === '') {
+            $('.error').html('Please check your entry.')
+        
+        } else {
+            reservationData.name = reservationName // Input for Name
+            reservationData.date = reservationDate // Input for Date
+
+            var reservationsReference = database.ref('reservations'); // Create a section in database
+            reservationsReference.push(reservationData);  // Push to this database reference
+
+            $('.reservation-form')[0].reset(); // Reset fields on submit
+            $('.error').remove();
+        }
+
+    });
+
+    
+    // PART THREE: RETRIEVE DATA & COMPILE HANDLEBARS //
+    function getReservations() { // retrieve reservations data
+
+        database.ref('reservations').on('value', function(results) { // use reference to database to listen for changes in reservations data
+
+            var allReservations = results.val(); // Get all reservations stored in the results we received back from database
+            $('.reservations').empty(); // remove all list reservations from DOM before appending list reservations
+
+            
+            for (var reservation in allReservations) { // iterate (loop) through all reservations coming from database call
+
+                // Create an object literal with the data we'll pass to Handlebars
+                var context = {
+                    name: allReservations[reservation].name, // {{name}}
+                    date: allReservations[reservation].date, // {{date}}
+                    reservationId: reservation //{{reservationId}}
+                };
+
+                var source = $("#reservation-template").html(); // Get the HTML from our Handlebars reservation template
+                var template = Handlebars.compile(source); // Compile our Handlebars template
+                var reservationListItem = template(context); // Pass the data for this reservation (context) into the template
+                $('.reservations').append(reservationListItem); // Append newly created reservation to reservations list.
+            }
+
+        });
+    }
+
+    // PART FOUR: TRIGGER PART THREE //
+    getReservations();
+
+    // DATEPICKER //
+    $(".reservation-date").datepicker({
+        dateFormat: 'd MM, y'
+    });
+
+
+// Google Map
 function initMap() {
     var monks = {
         lat: 40.8054491,
